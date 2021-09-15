5c4f657ecc2ecc3d088692f2b9212fc69f4e7ba6
PhanAnh1811 <tuananh.61779@gmail.com>
2021-09-15 08:26:14 +0700

    react-native

---
+++ src/assets/colors/colors.js
@@ -0,0 +1,8 @@
+const colors={
+    background:'#E5E5E5',
+    black:'#000000',
+    gray:'#666666',
+    blue:'#1C215D',
+    blueFaded:'#8D8FAD'
+}
+export default colors;
---
+++ src/assets/data/slide.js
@@ -0,0 +1,20 @@
+const slide =[
+    {
+        id:1,
+        title: 'Save time by tracking your studies',
+        text: 'Schedule your classes, assignments, quizzes and more',
+        image: require('../images/group1.png'),
+      },
+      {
+        id:2,
+        title: 'Stay on top of your education',
+        text: 'Reduce your stress, increase your productivity',
+        image: require('../images/group2.png'),
+      },
+      {
+        id:3,
+        title: 'Spend more time doing the things you love',
+        text: 'Get started within five minutes',
+        image: require('../images/group3.png'),
+      },
+]
